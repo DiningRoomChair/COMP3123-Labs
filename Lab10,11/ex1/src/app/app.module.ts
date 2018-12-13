eecc2824327b5cbeff21b7ef30b44dc703e61ccd
DiningRoomChair <43220681+DiningRoomChair@users.noreply.github.com>
2018-12-13 10:14:43 -0500

    more labs

--- Lab10,11/ex1/src/app/app.module.ts
+++ Lab10,11/ex1/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { RemoveSpacesPipe } from './remove-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    RemoveSpacesPipe
   ],
   imports: [
     BrowserModule,
--- Lab10,11/ex1/src/app/mock-heroes.ts
+++ Lab10,11/ex1/src/app/mock-heroes.ts
@@ -5,4 +5,6 @@ export const HEROES: Hero[] = [
     { id: 3, name: 'Sand King' },
     { id: 4, name: 'Furion' },
     { id: 5, name: 'Queen of Pain' },
+    { id: 18, name: 'The-Flash' },
+    { id: 19, name: 'Green-Lantern' }
 ]
